6b54c5b389c983c11b7a8be8e6883b7b15af1060
rcheuk <rcheuk@rcheuk-VirtualBox.(none)>
2014-01-29 21:13:42 -0500

    new files. attempt 3 0_o

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FuzzySpice::Application.config.secret_key_base = 'fc02bdb244a7b405c45da0b6d1e9641bd22dbb9a2aa21cbbe9ddaf133915105efa0caeb09af0337d709ba1d4f74e33490d86414c1cd1875a0c60fe5c99d58ac0'
